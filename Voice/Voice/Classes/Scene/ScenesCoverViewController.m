94b6a72d120105a87935734c8f5971cacccdb650
sh00008 <sh00008@sohu.com>
2011-07-26 10:27:51 +0800

    修正添加了CoverFlow之后，不能旋转屏幕的问题。

--- Voice/Voice/Classes/Scene/ScenesCoverViewController.m
+++ Voice/Voice/Classes/Scene/ScenesCoverViewController.m
@@ -227,7 +227,8 @@
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
     // Return YES for supported orientations
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+    return YES;
+    //return (interfaceOrientation == UIInterfaceOrientationPortrait);
 }
 
 - (void)loadScenes;
